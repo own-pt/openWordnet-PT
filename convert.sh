76ecb3f29c7235ab51d1b11276f0bbfc895bfe79
Alexandre Rademaker <arademaker@gmail.com>
2011-12-07 19:35:06 -0200

    Gerard new file and suggestions

--- convert.sh
+++ convert.sh
@@ -1,26 +1,34 @@
 
-split -a 1 -l 15000 uwn-pt-sorted.tsv uwn-pt-sorted-
-head -1 uwn-pt-sorted-a > header
+# before the script 
+# head -1 uwn-pt-sorted-a > header
+# edit header
 
-# add header
-for f in uwn-pt-sorted-[b-h]; do 
+split -a 2 -l 500 uwn-pt-sorted.tsv uwn-pt-sorted-
+
+# add header to all files without header
+tail +2 uwn-pt-sorted-aa > aa.tmp
+mv aa.tmp uwn-pt-sorted-aa
+for f in uwn-pt-sorted-??; do 
     cat header $f >> $f.new; 
     mv $f.new $f
 done
 
+wc -l uwn-pt-sorted-??
+wc -l uwn-pt-sorted.tsv
+
 # convert to XML
-for f in uwn-pt-sorted-?; do 
+for f in uwn-pt-sorted-??; do 
     python convert.py $f $f.xml 
 done
 
 # formating the XML
-for f in uwn-pt-sorted-?.xml; do
+for f in uwn-pt-sorted-??.xml; do
     tidy -utf8 -wrap 150 -xml -i $f 2> `basename $f .xml`.error > $f.new
     mv $f.new $f
 done
 
 # remove temp files
-rm uwn-pt-sorted-? header uwn-pt-sorted-?.error
+rm uwn-pt-sorted-?? uwn-pt-sorted-??.error
 
 
 
