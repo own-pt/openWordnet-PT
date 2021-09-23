fff856b529f6643b26c7584562a41e1750f58002
fredsonerd <fredsonerd@hotmail.com>
2021-09-23 16:06:22 -0300

    changing version 1.0.0 to 1.0

--- release.sh
+++ release.sh
@@ -7,8 +7,8 @@ python3 -m pyown.cli.statistics --ownpt data/own-pt-* --ownen data/own-en-* -v
 
 # generating LMFs
 wget https://raw.githubusercontent.com/globalwordnet/cili/master/ili-map.ttl
-python3 -m pyown.cli.lmf data/own-pt-* ili-map.ttl -li own-pt -lb OpenWordnet-PT -vr 1.0.0 -lg pt -cs 1.0 --email "arademaker@gmail.com" --url "http://openwordnet-pt.org/" --status "checked" --licence "http://creativecommons.org/licenses/by/4.0/" --citation "http://arademaker.github.io/bibliography/coling2012.html" -o own-pt-lmf-10.xml -v
-python3 -m pyown.cli.lmf data/own-en-* ili-map.ttl -li own-en -lb OpenWordnet-EN -vr 1.0.0 -lg en -cs 1.0 --email "arademaker@gmail.com" --url "http://openwordnet-pt.org/" --status "checked" --licence "http://creativecommons.org/licenses/by/4.0/" --citation "http://arademaker.github.io/bibliography/coling2012.html" -o own-en-lmf-10.xml -v
+python3 -m pyown.cli.lmf data/own-pt-* ili-map.ttl -li own-pt -lb OpenWordnet-PT -vr 1.0 -lg pt -cs 1.0 --email "arademaker@gmail.com" --url "http://openwordnet-pt.org/" --status "checked" --licence "http://creativecommons.org/licenses/by/4.0/" --citation "http://arademaker.github.io/bibliography/coling2012.html" -o own-pt-lmf-10.xml -v
+python3 -m pyown.cli.lmf data/own-en-* ili-map.ttl -li own-en -lb OpenWordnet-EN -vr 1.0 -lg en -cs 1.0 --email "arademaker@gmail.com" --url "http://openwordnet-pt.org/" --status "checked" --licence "http://creativecommons.org/licenses/by/4.0/" --citation "http://arademaker.github.io/bibliography/coling2012.html" -o own-en-lmf-10.xml -v
 
 # validanting LMFs
 wget https://globalwordnet.github.io/schemas/WN-LMF-1.1.dtd
