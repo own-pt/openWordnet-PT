96bd0bf729c508a6982be396440053a258252ca4
Alexandre Rademaker <arademaker@gmail.com>
2024-05-03 12:36:30 -0300

    improve backup script

--- dump/backup.sh
+++ dump/backup.sh
@@ -10,11 +10,10 @@ compact () {
 }
 
 ES_URL=5325493c-3489-4c1d-a81e-5db7cbaef410.8117147f814b4b2ea643610826cd2046.databases.appdomain.cloud:31366
-source secret.sh
-indexes=("suggestion" "audit" "wn" "votes")
 NODE_TLS_REJECT_UNAUTHORIZED=0
+source secret.sh
 
-for val in "${indexes[@]}"; do
+for val in "suggestion" "audit" "wn" "votes"; do
     elasticdump --input=https://$ES_USER:$ES_PASS@$ES_URL/$val --output=$val-new.json --type=data
 done
 
